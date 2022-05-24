1deb933276d76d15a71430046659d7259bdba569
Laurent <lbourassa95@gmail.com>
2022-05-24 19:51:05 -0400

    onlô

--- prob/src/prob.ino
+++ prob/src/prob.ino
@@ -17,8 +17,8 @@
 
 SYSTEM_THREAD(ENABLED);
 
-Data data;
-UART uart(MODE, &data);
+Data weatherData;
+UART uart(MODE, &weatherData);
 
 const size_t SCAN_RESULT_COUNT = 20;
 const unsigned long SCAN_PERIOD_MS = 2000;
@@ -32,6 +32,8 @@ BleUuid tempCharUUID("e2cd64e2-9ab3-4dd5-b40c-7dd6b62c16d5");
 BleUuid windDirCharUUID("3ee0aa80-8d59-4532-bdf1-7311f668de3a");
 BleUuid windSpeedCharUUID("4d4f0acc-47cd-4eb6-b1c7-83be045d377e");
 BleUuid rainCharUUID("0f381db9-9f9e-4190-b865-73f71ae01cb5");
+
+#if MODE == SLAVE
 BleCharacteristic lightCharacteristic("Light", BleCharacteristicProperty::NOTIFY, lightCharUUID, weatherStationUUID);
 BleCharacteristic baroPresCharacteristic("Baro Pres", BleCharacteristicProperty::NOTIFY, baroPresCharUUID, weatherStationUUID);
 BleCharacteristic baroTempCharacteristic("Baro Temp", BleCharacteristicProperty::NOTIFY, baroTempCharUUID, weatherStationUUID);
@@ -40,7 +42,17 @@ BleCharacteristic tempCharacteristic("Temp", BleCharacteristicProperty::NOTIFY, 
 BleCharacteristic windDirCharacteristic("Wind Dir", BleCharacteristicProperty::NOTIFY, windDirCharUUID, weatherStationUUID);
 BleCharacteristic windSpeedCharacteristic("Wind Speed", BleCharacteristicProperty::NOTIFY, windSpeedCharUUID, weatherStationUUID);
 BleCharacteristic rainCharacteristic("Rain", BleCharacteristicProperty::NOTIFY, rainCharUUID, weatherStationUUID);
-// BleCharacteristic baseCharacteristic;
+#else
+BleCharacteristic lightCharacteristic;
+BleCharacteristic baroPresCharacteristic;
+BleCharacteristic baroTempCharacteristic;
+BleCharacteristic humCharacteristic;
+BleCharacteristic tempCharacteristic;
+BleCharacteristic windDirCharacteristic;
+BleCharacteristic windSpeedCharacteristic;
+BleCharacteristic rainCharacteristic;
+#endif
+
 BlePeerDevice peer;
 
 #if MODE == SLAVE
@@ -51,14 +63,44 @@ BlePeerDevice peer;
   RainSensor rainSensor(A5);
 #endif
 
-// void baseCharHandler(const uint8_t* data, size_t len, const BlePeerDevice &peer, void* context)
-// {
-
-// }
-
 void lightCallback(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context)
 {
-  Serial.println("Light value changed!");
+  weatherData.lightData = *((int*)data);
+}
+
+void baroPresCallback(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context)
+{
+  weatherData.barPresData = *((float*)data);
+}
+
+void baroTempCallback(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context)
+{
+  weatherData.barTempData = *((float*)data);
+}
+
+void humCallback(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context)
+{
+  weatherData.humData = *((float*)data);
+}
+
+void tempCallback(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context)
+{
+  weatherData.tempData = *((float*)data);
+}
+
+void windDirCallback(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context)
+{
+  weatherData.windDirData = *((int*)data);
+}
+
+void windSpeedCallback(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context)
+{
+  weatherData.windSpeedData = *((float*)data);
+}
+
+void rainCallback(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context)
+{
+  weatherData.rainData = *((float*)data);
 }
 
 void setup() {
@@ -85,8 +127,6 @@ void setup() {
 
 #else
 
-  // baseCharacteristic.onDataReceived(baseCharHandler, &baseCharacteristic);
-
   while(!BLE.connected())
   {
     static unsigned long lastScan = 0;
@@ -105,10 +145,64 @@ void setup() {
 
           if (svcCount > 0 && foundServiceUUID == weatherStationUUID)
           {
-            Serial.write("Found you!");
+            Serial.println("Found you!");
             peer = BLE.connect(scanResult[i].address());
 
-            lightCharacteristic.onDataReceived(lightCallback, &lightCharacteristic);
+            if(peer.connected())
+            {
+              static Vector<BleCharacteristic> peerCharacteristics = peer.discoverAllCharacteristics();
+
+              for(int i = 0; i < peerCharacteristics.size(); i++)
+              {
+                if(peerCharacteristics[i].UUID() == lightCharUUID)
+                {
+                  peerCharacteristics[i].subscribe(true);
+                  peerCharacteristics[i].onDataReceived(lightCallback, &lightCharacteristic);
+                }
+
+                else if(peerCharacteristics[i].UUID() == baroPresCharUUID)
+                {
+                  peerCharacteristics[i].subscribe(true);
+                  peerCharacteristics[i].onDataReceived(baroPresCallback, &baroPresCharacteristic);
+                }
+
+                else if(peerCharacteristics[i].UUID() == baroTempCharUUID)
+                {
+                  peerCharacteristics[i].subscribe(true);
+                  peerCharacteristics[i].onDataReceived(baroTempCallback, &baroTempCharacteristic);
+                }
+
+                else if(peerCharacteristics[i].UUID() == humCharUUID)
+                {
+                  peerCharacteristics[i].subscribe(true);
+                  peerCharacteristics[i].onDataReceived(humCallback, &humCharacteristic);
+                }
+
+                else if(peerCharacteristics[i].UUID() == tempCharUUID)
+                {
+                  peerCharacteristics[i].subscribe(true);
+                  peerCharacteristics[i].onDataReceived(tempCallback, &tempCharacteristic);
+                }
+
+                else if(peerCharacteristics[i].UUID() == windDirCharUUID)
+                {
+                  peerCharacteristics[i].subscribe(true);
+                  peerCharacteristics[i].onDataReceived(windDirCallback, &windDirCharacteristic);
+                }
+
+                else if(peerCharacteristics[i].UUID() == windSpeedCharUUID)
+                {
+                  peerCharacteristics[i].subscribe(true);
+                  peerCharacteristics[i].onDataReceived(windSpeedCallback, &windSpeedCharacteristic);
+                }
+
+                else if(peerCharacteristics[i].UUID() == rainCharUUID)
+                {
+                  peerCharacteristics[i].subscribe(true);
+                  peerCharacteristics[i].onDataReceived(rainCallback, &rainCharacteristic);
+                }
+              }
+            }
 
             break;
           }
@@ -123,16 +217,29 @@ void loop() {
 #if MODE == SLAVE
   delay(3000);
   
-  data.lightData = lightSensor.getRaw();
-  lightCharacteristic.setValue(data.lightData);
+  weatherData.lightData = lightSensor.getRaw();
+  lightCharacteristic.setValue(weatherData.lightData);
 
-  data.barPresData = barSensor.getCompP(true);
-  data.barTempData = barSensor.getCompT(false);
-  data.humData = humSensor.getHumidity(true);
-  data.tempData = humSensor.getTemperature(false);
-  data.windDirData = windSensor.getDir();
-  data.windSpeedData = windSensor.getSpeed();
-  data.rainData = rainSensor.getRain();
+  weatherData.barPresData = barSensor.getCompP(true);
+  baroPresCharacteristic.setValue(weatherData.barPresData);
+
+  weatherData.barTempData = barSensor.getCompT(false);
+  baroTempCharacteristic.setValue(weatherData.barTempData);
+
+  weatherData.humData = humSensor.getHumidity(true);
+  humCharacteristic.setValue(weatherData.humData);
+
+  weatherData.tempData = humSensor.getTemperature(false);
+  tempCharacteristic.setValue(weatherData.tempData);
+
+  weatherData.windDirData = windSensor.getDir();
+  windDirCharacteristic.setValue(weatherData.windDirData);
+
+  weatherData.windSpeedData = windSensor.getSpeed();
+  windSpeedCharacteristic.setValue(weatherData.windSpeedData);
+
+  weatherData.rainData = rainSensor.getRain();
+  rainCharacteristic.setValue(weatherData.rainData);
 
   // if(uart.checkRequest())
   // {
@@ -149,13 +256,14 @@ void loop() {
   delay(5000);
   // uart.requestData();
 
-  Serial.printlnf("Light: %d", data.lightData);
-  Serial.printlnf("Pressure: %.2f", data.barPresData);
-  Serial.printlnf("Baro Temperature: %.2f", data.barTempData);
-  Serial.printlnf("Humidity: %.2f", data.humData);
-  Serial.printlnf("Temperature: %.2f", data.tempData);
-  Serial.printlnf("Wind Direction: %d", data.windDirData);
-  Serial.printlnf("Wind Speed: %.2f", data.windSpeedData);
-  Serial.printlnf("Rain: %.2f", data.rainData);
+  Serial.println("------------------------------------------------");
+  Serial.printlnf("Light: %d", weatherData.lightData);
+  Serial.printlnf("Pressure: %.2f", weatherData.barPresData);
+  Serial.printlnf("Baro Temperature: %.2f", weatherData.barTempData);
+  Serial.printlnf("Humidity: %.2f", weatherData.humData);
+  Serial.printlnf("Temperature: %.2f", weatherData.tempData);
+  Serial.printlnf("Wind Direction: %d", weatherData.windDirData);
+  Serial.printlnf("Wind Speed: %.2f", weatherData.windSpeedData);
+  Serial.printlnf("Rain: %.2f", weatherData.rainData);
 #endif
 }
